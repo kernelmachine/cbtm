4fc854c8ad4f71f5cd5909aee210f36e3675b351
Margaret Li <hadasah@gmail.com>
2023-03-25 23:12:47 -0400

    Update consolidate_fsdp_shards.sh

--- metaseq/scripts/consolidate_fsdp_shards.sh
+++ metaseq/scripts/consolidate_fsdp_shards.sh
@@ -20,4 +20,4 @@ done
 
 echo "Consolidating ${count} models"
 
-/gscratch/zlab/sg01/bin/parallel --link --ungroup --jobs 10 python -m metaseq.scripts.consolidate_fsdp_shards {1} --save-prefix {2} --new-arch-name transformer_lm_gpt ::: $ORIGINAL_MODEL_PATHS ::: $CONSOLIDATED_MODEL_PREFIXES
+parallel --link --ungroup --jobs 10 python -m metaseq.scripts.consolidate_fsdp_shards {1} --save-prefix {2} --new-arch-name transformer_lm_gpt ::: $ORIGINAL_MODEL_PATHS ::: $CONSOLIDATED_MODEL_PREFIXES
